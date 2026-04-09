9444c9a09b492e57a908f91a511751e2745b90aa
Friedemann12 <julian.friedemann@googlemail.com>
2026-04-09 16:22:23 +0200

    fix

--- src/Medical_Wizard_MCP/server.py
+++ src/Medical_Wizard_MCP/server.py
@@ -1,13 +1,16 @@
 import os
-
+import asyncio
 from fastmcp import FastMCP
 
 mcp = FastMCP(
     name="Medical Wizard MCP",
-    host=os.getenv("MCP_HOST", "127.0.0.1"),
-    port=int(os.getenv("MCP_PORT", "8000")),
-    streamable_http_path=os.getenv("MCP_MOUNT_PATH", "/mcp"),
-    stateless_http=True,
-    json_response=True,
     version=os.getenv("MCP_VERSION", "local"),
 )
+
+if __name__ == "__main__":
+    asyncio.run(mcp.run_http_async(
+        host=os.getenv("FASTMCP_HOST", "0.0.0.0"),
+        port=int(os.getenv("FASTMCP_PORT", "8000")),
+        stateless_http=True,
+        json_response=True,
+    ))
